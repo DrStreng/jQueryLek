f5216b9e90f2b90b8100d3d336ac5fd1e6687cd1
Strengol <Strengol1@gmail.com>
2015-11-22 01:16:23 +0100

    new GUI

--- WebContent/js/test.js
+++ WebContent/js/test.js
@@ -1,97 +1,16 @@
 $(document).ready(function () {
 
-    $('.nav li').hover(function () { 
-    	$('ul', this).fadeIn();},function () {
-    		$('ul', this).fadeOut();
-	});
-
-    $("#imie").inputFocus('setText',{text:"podaj imie"}).inputFocus('setRequired');
-    $("#nazwisko").inputFocus('setText',{text:"podaj Nazwisko"}).inputFocus('setRequired');
-
-    $(".hide").click(function(){
-		$("h1").hide();
-	});
-
-	$('.background').hover(function(){
-  		$("h1").toggleClass('hoverh1');
-	});
-
-	$('.background').hover(function(){
-  		$(".logo").toggleClass('test');
-	});
-
-	$('.backgroundper').click(function(){
-  		$("h1").toggleClass('toggleClassh1');
-	});
-	
-	$(".show").click(function(){
-		$("h1").show();
-	});
-	
-	$(".fadeIn").click(function(){
-		$("h1").fadeIn();
-	});
-	
-	$(".fadeOut").click(function(){
-		$("h1").fadeOut();
-	});
-
-	$(".fadeToggle").click(function(){
-		$("h1").fadeToggle();
-	});
-
-	$(".reg").click(function(){
-		$("h1").hide();
-		$("h3").hide();
-		$("h2").hide();
-		$("h4").hide();
-		$("h5").slideDown();
-		$(".register").slideDown();
-
-	});
-
-	$(".1").click(function(){
-		$("h1").hide();
-		$("h3").hide();
-		$("h4").hide();
-		$(".register").hide();
-		$("h2").slideDown();
-	});
-
-	$(".2").click(function(){
-		$("h1").hide();
-		$("h2").hide();
-		$("h4").hide();
-		$(".register").hide();
-		$("h3").slideDown();
-	});
-
-	$(".3").click(function(){
-		$("h1").hide();
-		$("h3").hide();
-		$("h2").hide();
-		$(".register").hide();
-		$("h4").slideDown();
-	});
-	
-	$(".home1").click(function(){
-		$("h1").hide();
-		$("h3").hide();
-		$("h2").hide();
-		$(".register").hide();
-		$("h4").hide();
-	});
-
-	$(".home2").click(function(){
-		$("h1").hide();
-		$("h3").hide();
-		$("h2").hide();
-		$(".register").hide();
-		$("h4").hide();
-	});
-
-	$("button").click(function(){
-        $("#p1").css("color", "red").slideUp(1000).slideDown(1000);
+  $(".nav li").hover(function(){
+    $(".down",this).toggleClass("down2").css("color","#1ABC9C");
+    $(".border",this).slideDown();
+    $(".nav2 li").hover(function(){
+      $("ul",this).slideDown();
+      },function(){
+        $("ul",this).hide();
     });
-});
+    },function(){
+      $(".down",this).toggleClass("down2").css("color","#fff");
+      $(".border",this).hide();
+  });
 
+});
